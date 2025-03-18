c61b4b6cc2141fa1e2f60e51ccf4e379cf72a682
whatever <whateverman0026@gmail.com>
2025-03-18 21:27:06 +0300

    deneme

--- broken_access_control/1.py
+++ broken_access_control/1.py
@@ -2,11 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def main_page():
-    return 'Hoşgeldiniz.'
-
-@app.route('/user/<int:id>')
-def profile(id):
+@app.route('/settings/<int:user_id>')
+def profile(user_id):
     # Erişim kontrolü problem örneği
-    return '{0} nolu id degerine sahip user'.format(id)
+    return '{0} nolu id degerine sahip kullanici ayar sayfasi'.format(user_id)
